636a4cd9a847f0b9a725700d97daec1ed0b4837c
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 13:31:28 +0100

    event part som är ommouseover träning

---
+++ main.js
@@ -0,0 +1,15 @@
+/* gobal cosole, alert, prompt*/
+
+var myDiv = document.getElementById("div");
+
+myDiv.onmouseover = function(){
+
+"use strict";
+
+  myDiv.innerHTML = " You Hovered On Me";
+
+
+};
+
+
+
